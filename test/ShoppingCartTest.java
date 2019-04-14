134eb117a8a1eb7b815c5bedf4ff9ab927d09163
semsettin <ahmetozdemirden@std.iyte.edu.tr>
2019-04-14 12:41:08 +0300

    initial commit.

---
+++ test/ShoppingCartTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class ShoppingCartTest {
+
+    @Test
+    public void givenShoppingCartWithProductsAndDiscountsWhenGetTotalAmountAfterDiscountsCalledThenItShouldReturnTotalAmount() {
+
+    }
+
+}
